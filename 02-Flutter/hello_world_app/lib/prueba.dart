bf7ea4297dd56712c6b345bfdc4e3735e2215784
SeekerRafael <rafael.ariasf03@gmail.com>
2025-07-02 23:39:01 -0700

    Nuevo ejemplo donde ingreso un nombre y muestra un saludo

--- 02-Flutter/hello_world_app/lib/prueba.dart
+++ 02-Flutter/hello_world_app/lib/prueba.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SaludoApp());
 }
 
-class MyApp extends StatelessWidget {
+class SaludoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hola Flutter'),
-        ),
-        body: Center(
-          child: Text('¡Hola mundo!'),
+      home: SaludoScreen(),
+    );
+  }
+}
+
+class SaludoScreen extends StatefulWidget {
+  @override
+  _SaludoScreenState createState() => _SaludoScreenState();
+}
+
+class _SaludoScreenState extends State<SaludoScreen> {
+  String nombre = '';
+  String mensaje = '';
+
+  final controlador = TextEditingController();
+
+  void saludar() {
+    setState(() {
+      mensaje = '¡Hola, $nombre!';
+    });
+  }
+
+  @override
+  void dispose() {
+    controlador.dispose(); // limpiar al cerrar
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Saludo App')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controlador,
+              decoration: InputDecoration(labelText: 'Escribe tu nombre'),
+              onChanged: (valor) {
+                setState(() {
+                  nombre = valor;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: saludar,
+              child: Text('Saludar'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              mensaje,
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
         ),
       ),
     );
