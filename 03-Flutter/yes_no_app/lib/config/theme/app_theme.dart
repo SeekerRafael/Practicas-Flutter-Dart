1558fe97431a5caf7501e792d85c0aad82ef12f6
SeekerRafael <rafael.ariasf03@gmail.com>
2025-07-16 22:30:45 -0700

    agregando mas colores para el tema

--- 03-Flutter/yes_no_app/lib/config/theme/app_theme.dart
+++ 03-Flutter/yes_no_app/lib/config/theme/app_theme.dart
@@ -10,24 +10,31 @@ const List<Color> _colorThemes = [
   Colors.orangeAccent,
   Colors.tealAccent,
   Colors.pink,
-
+  Colors.green,
+  Colors.cyan,
+  Colors.indigo,
+  Colors.deepPurple,
+  Colors.lightGreen,
+  Color.fromARGB(255, 255, 140, 0), // deep orange
+  Color.fromARGB(255, 0, 188, 212), // cyan accent
+  Color.fromARGB(255, 156, 39, 176), // purple
+  Color.fromARGB(255, 63, 81, 181), // indigo
 ];
 
 class AppTheme {
-
   final int selectedColor;
 
   AppTheme({
-    this.selectedColor = 0
-  }): assert (selectedColor >= 0 && selectedColor <= _colorThemes.length-1, 
-          'Solo puedes seleccionar colores desde el 0 al ${_colorThemes.length}');
-  
+    this.selectedColor = 0,
+  }) : assert(
+          selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+          'Solo puedes seleccionar colores desde el 0 al ${_colorThemes.length - 1}',
+        );
 
   ThemeData theme() {
     return ThemeData(
       colorSchemeSeed: _colorThemes[selectedColor],
-      //brightness: Brightness.dark,
+      // brightness: Brightness.dark,
     );
   }
 }
-
