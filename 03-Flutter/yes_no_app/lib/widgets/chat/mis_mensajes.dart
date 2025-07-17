a560e7fe066d7726a9f5f8912c983f8c20b49326
SeekerRafael <rafael.ariasf03@gmail.com>
2025-07-16 22:32:05 -0700

    nueva version mejora y limpia en MisMensajes

--- 03-Flutter/yes_no_app/lib/widgets/chat/mis_mensajes.dart
+++ 03-Flutter/yes_no_app/lib/widgets/chat/mis_mensajes.dart
@@ -2,34 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class MisMensajes extends StatelessWidget {
-
   final Message message;
 
-  const MisMensajes({super.key, required, required this.message});
+  const MisMensajes({
+    super.key,
+    required this.message,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
 
-    final colors = Theme.of(context).colorScheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: colors.primary,
-            borderRadius: BorderRadius.circular(20)
+            color: colorScheme.primary,
+            borderRadius: BorderRadius.circular(20),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text(message.text, style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+            message.text,
+            style: const TextStyle(color: Colors.white),
           ),
         ),
-        const SizedBox(height: 10,)
-
+        const SizedBox(height: 10),
       ],
     );
-
-
-
   }
 }
